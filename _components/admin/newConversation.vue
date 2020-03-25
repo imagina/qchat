e2fb8aa18dc97e89112d28367ba3c8b1435d3599
YeisonTapia <yftapial@ut.edu.co>
2020-03-25 17:24:04 -0500

    Add loading when a message is send in a new conversation, Update functions for create conversation, Update function for send message in a conversation.

--- _components/admin/newConversation.vue
+++ _components/admin/newConversation.vue
@@ -6,9 +6,14 @@
       @keyup.enter="handlerClick()"
       outlined
       v-model="message"
-      :placeholder="`Enviar mensage a ${user.firstName}`">
+      :placeholder="`Enviar mensage a @${user.firstName}`">
       <template v-slot:append>
+        <q-spinner-comment
+          v-if="loading"
+          color="primary"
+          size="sm" />
         <q-btn
+          v-else
           flat
           @click="handlerClick"
           size="sm"
@@ -31,21 +36,24 @@
     },
     data(){
       return {
+        loading: false,
         message: '',
+        conversation: {},
       }
     },
     methods:{
       async handlerClick(){
         if(this.message){
-          let conversation = []
-           conversation = await this.getConversation()
+          this.loading = true
+          let conversation = {}
+          conversation = await this.getConversation()
           console.warn(conversation)
           return
-          //  ----------
           if (conversation.length == 0){
             conversation = await this.createConversation()
           }
           await this.sendMessage(conversation)
+          this.loading = false
         }
       },
       async getConversation(){
@@ -72,7 +80,13 @@
         return conversation
       },
       async createConversation() {
-        let form = {}
+        let form = {
+          private: false,
+          users: [
+            this.user.id,
+            this.$store.state.quserAuth.userId
+          ]
+        }
         let conversation = {}
         await this.$crud.create('apiRoutes.qchat.conversations', form).then( ({data}) => {
           conversation = data
@@ -85,11 +99,20 @@
         return conversation
       },
       async sendMessage(conversation){
-        let form = {}
-        await this.$crud.create('apiRoutes.qchat.conversations', form).then( ({data}) => {
+        let form = {
+          body: this.message,
+          userId: this.$store.state.quserAuth.userId,
+          conversationId: conversation.id
+        }
+        await this.$crud.create('apiRoutes.qchat.messages', form).then( ({data}) => {
           this.message = ''
-          // redirect to conversation
-          // hide users panel
+          this.$root.$emit('newNewConversationMessage', this.conversation)
+          this.$router.push({
+            name: 'qchat.admin.conversation.show',
+            params:{
+              id: conversation.id
+            }
+          })
         }).catch( error => {
           this.$alert.error({
             message: this.$tr('ui.message.errorRequest'),
