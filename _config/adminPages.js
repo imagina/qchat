08e7189f8921d4560b195458f2e30e8dd54d6b93
imagina <msolano2505@gmail.com>
2023-04-10 15:40:12 -0500

    +Update: Icons

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -7,7 +7,7 @@ export default {
     page: () => import('@imagina/qchat/_pages/main/conversations'),
     layout: () => import('@imagina/qsite/_layouts/master.vue'),
     title: 'ichat.cms.sidebar.conversations',
-    icon: 'far fa-comments',
+    icon: 'fa-light fa-messages',
     authenticated: true,
     subHeader: {
       refresh: true
